17525aacc6181ca3904c6e6a29d6d58d0fb9ee7b
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 21:15:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3, :sides
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @sides = []
+    @sides << side_1 << side_2 << side_3
+  end
+  
+  def kind
+    if @sides.max >= (@sides.min(2)[0] + @sides.min(2)[1]) || @sides.any?{|s| s <= 0}
+      raise TriangleError
+    else
+      if @sides.uniq.length == 1
+        return :equilateral
+      elsif @sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+  
+  class TriangleError < StandardError
+    def alert
+      puts "You are not a triangle!"
+    end
+  end
 end
