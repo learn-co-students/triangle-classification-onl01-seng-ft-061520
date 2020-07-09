9e7e0e7bd3f4ca369846e39f0845a164e0c69975
Soundwood <j.benhogan@gmail.com>
2020-07-09 08:02:31 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :a, :b, :c, :kind
+  def initialize(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a+b <= c || a+c <= b || b+c <= a
+      raise TriangleError
+    else
+      if a == b && b == c
+        @kind = :equilateral
+      elsif a == b || b == c || a == c
+        @kind = :isosceles
+      elsif a != b && b != c && a != c
+        @kind = :scalene
+      end
+    end
+  end
+ 
+  class TriangleError < StandardError
+    "This triangle is not legal"
+  end
 end
